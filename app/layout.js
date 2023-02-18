5253edc7d097acaa1c1ca25cfdc3ff934df27585
Bon Aguilan <aguilanbon@gmail.com>
2023-02-18 21:45:01 +0800

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 
 export default function RootLayout({ children }) {
@@ -8,7 +9,15 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <nav>
+          <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
